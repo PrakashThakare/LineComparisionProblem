9c76a97d47787544ba60f4ce79547352390c6400
Shree Thakre <shreet365@gmail.com>
2022-12-20 22:06:15 +0530

    Updating Program Comapring Two Line

--- LineComparisionProblem/Program.cs
+++ LineComparisionProblem/Program.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Security.Cryptography.X509Certificates;
 using System.Text;
 using System.Threading.Tasks;
 
@@ -14,16 +15,39 @@ namespace LineComparisionProblem
 
             Console.Write("Enter Starting Point: ");
             Double x1 = Convert.ToDouble(Console.ReadLine());
+
             Console.Write("Enter Ending Point: ");
             Double x2 = Convert.ToDouble(Console.ReadLine());
 
             Console.Write("Enter Starting Point: ");
-            Double y1= Convert.ToDouble(Console.ReadLine());
+            Double y1 = Convert.ToDouble(Console.ReadLine());
+
             Console.Write("Enter Ending Point: ");
             Double y2 = Convert.ToDouble(Console.ReadLine());
 
-            Double Length = Math.Sqrt(Math.Pow((x2 - x1), 2) + (Math.Pow((y2 - y1), 2)));
-            Console.WriteLine("Length is : "+Length);
+            Console.Write("Enter Ending Point: ");
+            Double x3 = Convert.ToDouble(Console.ReadLine());
+
+            Console.Write("Enter Ending Point: ");
+            Double x4 = Convert.ToDouble(Console.ReadLine());
+
+            Console.Write("Enter Starting Point: ");
+            Double y3 = Convert.ToDouble(Console.ReadLine());
+
+            Console.Write("Enter Ending Point: ");
+            Double y4 = Convert.ToDouble(Console.ReadLine());
+
+            Double Length1 = Math.Sqrt(Math.Pow((x2 - x1), 2) + (Math.Pow((y2 - y1), 2)));
+            Double Length2 = Math.Sqrt(Math.Pow((x4 - x3), 2) + (Math.Pow((y3 - y4), 2)));
+
+            if (Length1 == Length2)
+            {
+                Console.WriteLine("Length is Equal : " + Length1);
+            }
+            else
+            {
+                Console.WriteLine("Length is Not Equal : " + Length1 + " \n"+ Length2);
+            }
 
             Console.ReadLine();
         }
